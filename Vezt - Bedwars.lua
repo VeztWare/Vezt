da7f98c14649a34fee70006a91d1c9d2a98140d0
VeztWare <117451997+VeztWare@users.noreply.github.com>
2023-06-10 15:47:21 +0100

    End of vezt

--- Vezt - Bedwars.lua
+++ Vezt - Bedwars.lua
@@ -1,2 +1,3 @@
-game.Players.LocalPlayer:Kick("Sorry, Vezt is currently patched.\nWe're making things more awesome!")
+setclipboard("https://discord.gg/4pea5KgBey")
+game.Players.LocalPlayer:Kick("Sorry, Vezt is currently patched. (discord copied to clipboard).\nWe're making things more awesome!")
 
